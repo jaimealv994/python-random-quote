d0636c0381c272c8029e5afb0c25aa1e62755d1a
Jaime Noel Alvarez Luna <jaimealv994@gmail.com>
2020-03-27 22:12:11 -0600

    adding quotes programmatically

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,21 @@
+import datetime
 import random
+
+
+def get_random_quote(quotes):
+    rnd = random.randint(0, len(quotes) - 1)
+    return quotes[rnd].rstrip("\n")
 
 
 def entry():
     # print("Keep it logically awesome.")
 
-    f = open("quotes.txt")
+    f = open("quotes.txt", "r+")
+    f.write("Dynamic quote at {0}\n".format(datetime.date.today().strftime("%c")))
     quotes = f.readlines()
     f.close()
-
-    rnd = random.randint(0, len(quotes) - 1)
-
-    print(quotes[rnd])
+    print(get_random_quote(quotes))
+    print(get_random_quote(quotes))
 
 
 if __name__ == "__main__":
